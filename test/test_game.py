f34166900edae6c90c91fdc42020cbf93ed763b1
may <4721319+mayshukla@users.noreply.github.com>
2022-03-22 21:50:11 -0600

    Add unit test for Game.tick

--- test/test_game.py
+++ test/test_game.py
@@ -8,6 +8,9 @@ class TestGame(unittest.TestCase):
         self.game = Game([])
         self.client = MagicMock()
 
+    def mock_agents(self):
+        self.game.agents = [MagicMock(), MagicMock()]
+
     def test_exec_player_code_good(self):
         player_code = """
 class MyAgent(Agent):
@@ -32,6 +35,14 @@ class MyAgent(Agent):
         """
         success = self.game.exec_player_code(self.client, player_code, "MyAgent")
         assert(not success)
+
+    def test_tick(self):
+        self.mock_agents()
+        self.game.tick()
+        for agent_mock in self.game.agents:
+            agent_mock._tick.assert_called()
+
+        # TODO add more asserts as we add functionality to Game.tick
 
 
 if __name__ == '__main__':
