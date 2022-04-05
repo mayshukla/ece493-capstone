f3a12690473a6dc6227f5016dd842a3b912761b4
may <4721319+mayshukla@users.noreply.github.com>
2022-04-05 12:21:52 -0600

    Make Agent1 maintain distance from Agent2

--- test/agent_code/agent2.py
+++ test/agent_code/agent2.py
@@ -11,7 +11,8 @@ class Agent2(Agent):
     def run(self):
         Agent2.random_movement_timer -= 1
         if Agent2.random_movement_timer <= 0:
-            self.randomize_movement()
+            self.randomize_angle()
+            self.set_movement_direction(Agent2.angle)
             Agent2.random_movement_timer = 30 * 2
             Agent2.random_cooldown_timer = 30
         if Agent2.random_cooldown_timer > 0:
@@ -21,9 +22,8 @@ class Agent2(Agent):
         if Agent2.random_cooldown_timer <= 0:
             self.set_movement_direction(Agent2.angle)
 
-    def randomize_movement(self):
+    def randomize_angle(self):
         Agent2.angle = random.random() * 360
-        self.set_movement_direction(Agent2.angle)
 
     def on_obstacle_hit(self):
         # Change direction when an obstacle is hit
@@ -33,8 +33,8 @@ class Agent2(Agent):
     def on_enemy_scanned(self, enemy_position):
         self.activate_shield()
         # shoot towards enemy when they are near
-        distance = (enemy_position.x - self.get_position().x, enemy_position.y - self.get_position().y)
-        angle = math.degrees(math.atan2(distance[1], distance[0]))
+        rel_position = (enemy_position.x - self.get_position().x, enemy_position.y - self.get_position().y)
+        angle = math.degrees(math.atan2(rel_position[1], rel_position[0]))
         self.attack_ranged(angle)
         # run away from the enemy!
         Agent2.angle = 180 - angle
